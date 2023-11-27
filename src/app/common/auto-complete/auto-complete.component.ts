a8bf63df9a63ce5a323532af1a9d6383876f1ff7
Gokula <gokulakannanit@gmail.com>
2023-11-27 08:20:28 +0530

    'add'

--- src/app/common/auto-complete/auto-complete.component.ts
+++ src/app/common/auto-complete/auto-complete.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Observable, map, startWith } from 'rxjs';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'auto-complete',
@@ -15,24 +17,44 @@ import { MatInputModule } from '@angular/material/input';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatAutocompleteModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   templateUrl: './auto-complete.component.html',
   styleUrls: ['./auto-complete.component.css']
 })
 export class AutoCompleteComponent {
+
+  @ViewChild('inputtxt')
+  private inputtxt:any;
+
+
   @Input() label: string = '';
-  myControl = new FormControl('');
+
+  myControl = new FormControl();
 
   @Input() options: any[] = [];
 
   filteredOptions: Observable<any[]>;
 
   ngOnInit() {
+    this.initiateFilter();
+  }
+
+  private initiateFilter() {
+    this.filteredOptions = new Observable();
+    this.filteredOptions.pipe();
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       map(value => this._filter(value || '', this.options)),
     );
+  }
+  clearSelection() {
+    this.inputtxt.nativeElement.focus();
+    this.inputtxt.value='';
+    this.myControl.setValue(null);
+    this.initiateFilter();
   }
 
   private _filter(value: string, options: any[]): any[] {
